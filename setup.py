f013ac41856990381a675229a3ed5291829c4bb5
Fernando Perez <fernando.perez@berkeley.edu>
2021-09-30 06:31:52 +0000

    Only require py36

--- setup.py
+++ setup.py
@@ -23,6 +23,6 @@ setuptools.setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    python_requires  = '>= 3.7',
+    python_requires  = '>= 3.6',
     install_requires = requirements,
 )
