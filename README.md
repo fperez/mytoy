9f7cdd90c3a646677160c32b10790e7bc90b89a6
Fernando Perez <Fernando.Perez@berkeley.edu>
2022-04-06 06:43:01 +0000

    Update environment/setup to more modern standards.
    Use setup.cfg and pyproj.toml for build/install setup, and
    environment.yml for full environment configuration.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ It has a single source directory (`mytoy`) with an `__init__.py` file and one "i
 
 In addition to this `README.md` it includes some basic infrastructure: `LICENSE`, `requirements.txt`, `setup.py` and `.gitignore` files.
 
-This is more or less the absolute minimum for a "real" python package that can be installed from source, tested and experimented with on Binder.
+This is more or less the absolute minimum for a "real" python package that can be installed from source, tested and experimented with on Binder.  For a more "official" version of this same idea, see the [PyPA sample project repo](https://github.com/pypa/sampleproject).
 
 The only docs included are this `README.md` file - a larger package would have a proper docs directory and associated Sphinx/JupyterBook build.
 
--- setup.py
+++ setup.py
@@ -1,28 +1,12 @@
 # A minimal setup.py file to make a Python project installable.
+#
+# Note that while we are following modern packaging practices
+# with setuptools metadata being declaratively stored in setup.cfg
+# and build configuration listed in pyproj.toml, pip/setuptools
+# as of this writing (early 2022) still require a minimal setup.py
+# file in order to support editable development installs (pip install -e .)
 
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-with open("requirements.txt", "r") as fh:
-    requirements = [line.strip() for line in fh]
-
-
-setuptools.setup(
-    name             = "mytoy",
-    version          = "0.0.1",
-    author           = "My Name",
-    author_email     = "me@myemail.com",
-    description      = "A Python library to make toys.",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
-    packages         = setuptools.find_packages(),
-    classifiers       = [
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires  = '>= 3.6',
-    install_requires = requirements,
-)
+if __name__ == "__main__":
+	setup()
